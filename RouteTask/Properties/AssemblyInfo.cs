d90953415bd5273040c07233983b59d54008c1d0
Ilya Chumakov <ilya.chumakov99@gmail.com>
2020-04-01 13:20:20 +0300

    folder renamed

--- RouteTask/Properties/AssemblyInfo.cs
+++ RouteTask/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("SimplexMethod")]
+[assembly: AssemblyTitle("RouteTask")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimplexMethod")]
+[assembly: AssemblyProduct("RouteTask")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ RouteTask/Vogel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplexMethod
+{
+    public class Vogel : AbstractSolver
+    {
+        public Vogel(List<List<object>> values, object[] requests, object[] stocks) : base(values, requests, stocks)
+        {
+
+        }
+
+        protected override void processBasis()
+        {
+            
+        }
+    }
+}
